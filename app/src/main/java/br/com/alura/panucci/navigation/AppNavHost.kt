c3eee741758e04090671943febaf9029b3b35d06
Lueny Dantas <luenypaixao@gmail.com>
2023-05-04 22:00:01 -0300

    Using nested graph navigation

--- app/src/main/java/br/com/alura/panucci/navigation/AppNavHost.kt
+++ app/src/main/java/br/com/alura/panucci/navigation/AppNavHost.kt
@@ -1,50 +1,22 @@
 package br.com.alura.panucci.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.navOptions
-import br.com.alura.panucci.ui.components.BottomAppBarItem
 
 @Composable
 fun AppNavHost(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = highLightsRoute
+        startDestination = homeGraphRoute
     ) {
-        highLightsScreen(navController)
-        menuScreen(navController)
-        drinksScreen(navController)
+        homeGraph(navController)
         productDetailsScreen(navController)
         checkoutScreen(navController)
 
     }
 }
 
-fun NavController.navigateItemWithPopUpTo(
-    item: BottomAppBarItem
-) {
-    val (route, navigate) = when (item) {
-        BottomAppBarItem.Drinks -> Pair(
-            drinksRoute,
-            ::navigateToDrinks
-        )
-        BottomAppBarItem.Highlights -> Pair(
-            highLightsRoute,
-            ::navigateToHighLights
-        )
-        BottomAppBarItem.Menu -> Pair(
-            menuRoute,
-            ::navigateToMenu
-        )
-    }
-    val navOptions = navOptions {
-        launchSingleTop = true
-        popUpTo(route)
-    }
-    navigate(navOptions)
-}
 
 
 
---
+++ app/src/main/java/br/com/alura/panucci/navigation/homeGraphNavigation.kt
@@ -0,0 +1,45 @@
+package br.com.alura.panucci.navigation
+
+import androidx.navigation.*
+import br.com.alura.panucci.ui.components.BottomAppBarItem
+
+internal const val  homeGraphRoute = "home"
+
+fun NavGraphBuilder.homeGraph(navController: NavHostController) {
+    navigation(
+        startDestination = highLightsRoute,
+        route = " home"
+    ) {
+        highLightsScreen(navController)
+        menuScreen(navController)
+        drinksScreen(navController)
+    }
+}
+
+fun NavController.navigateToHomeGraph(){
+    navigate(homeGraphRoute)
+}
+
+fun NavController.navigateItemWithPopUpTo(
+    item: BottomAppBarItem
+) {
+    val (route, navigate) = when (item) {
+        BottomAppBarItem.Drinks -> Pair(
+            drinksRoute,
+            ::navigateToDrinks
+        )
+        BottomAppBarItem.Highlights -> Pair(
+            highLightsRoute,
+            ::navigateToHighLights
+        )
+        BottomAppBarItem.Menu -> Pair(
+            menuRoute,
+            ::navigateToMenu
+        )
+    }
+    val navOptions = navOptions {
+        launchSingleTop = true
+        popUpTo(route)
+    }
+    navigate(navOptions)
+}
