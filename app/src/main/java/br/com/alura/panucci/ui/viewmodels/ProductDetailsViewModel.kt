b8007701ebce86ed55dbec46a1743f0c2efe432c
Lueny Dantas <luenypaixao@gmail.com>
2023-05-07 20:08:58 -0300

    Saved State Handle in ViewModel

--- app/src/main/java/br/com/alura/panucci/ui/viewmodels/ProductDetailsViewModel.kt
+++ app/src/main/java/br/com/alura/panucci/ui/viewmodels/ProductDetailsViewModel.kt
@@ -1,24 +1,39 @@
 package br.com.alura.panucci.ui.viewmodels
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import br.com.alura.panucci.dao.ProductDao
+import br.com.alura.panucci.navigation.productIdArgument
 import br.com.alura.panucci.ui.uistate.ProductDetailsUiState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class ProductDetailsViewModel(
-    private val dao: ProductDao = ProductDao()
+    private val dao: ProductDao,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow<ProductDetailsUiState>(
         ProductDetailsUiState.Loading
     )
     val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            savedStateHandle
+                .getStateFlow<String?>(productIdArgument, null)
+                .filterNotNull()
+                .collect { id ->
+                    findProductById(id)
+                }
+        }
+    }
 
     fun findProductById(id: String) {
         _uiState.update { ProductDetailsUiState.Loading }
@@ -32,4 +47,16 @@ class ProductDetailsViewModel(
             _uiState.update { dataState }
         }
     }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                ProductDetailsViewModel(
+                    dao = ProductDao(),
+                    savedStateHandle = createSavedStateHandle()
+                )
+            }
+        }
+    }
+
 }
